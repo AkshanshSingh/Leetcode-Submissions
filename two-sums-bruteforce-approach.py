e12765a86e9a332251fe7f1772f70304806c4d28
Akshansh <43261823+AkshanshSingh@users.noreply.github.com>
2021-01-11 16:24:57 +0530

    Update two-sums-bruteforce-approach.py

--- two-sums-bruteforce-approach.py
+++ two-sums-bruteforce-approach.py
@@ -4,9 +4,9 @@ class Solution:
         self.j = 0
         self.op = []
         for self.i in range(len(nums)):
-            for self.j in range(len(nums)):
-                if self.j == self.i:
-                    continue
+            for self.j in range(self.i+1,len(nums)):
+                #if self.j == self.i:
+                #    continue
                 if nums[self.j] + nums[self.i] == target:
                     self.op.append(self.i)
                     self.op.append(self.j)
